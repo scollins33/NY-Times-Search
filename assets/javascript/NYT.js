9c344018e392e2e98682548b96e8da9e9818de25
kimberly owens <kimberlyowens@lawn-128-61-44-139.lawn.gatech.edu>
2017-09-16 13:29:27 -0400

    kims shit to break seans

--- assets/javascript/NYT.js
+++ assets/javascript/NYT.js
@@ -1,5 +1,11 @@
+
+var searchTerm = 'ronald reagan'
+var recordNumber = 0;
+var startYear= '19000101';
+var endYear = getEndYear();
+var today = new Date();
+
 function getEndYear () {
-    var today = new Date();
     var thisDay = today.getDate();
     var thisMonth = today.getMonth();
     var thisYear = today.getFullYear();
@@ -13,7 +19,35 @@ function getEndYear () {
     }
 
     var endYear = thisYear + thisMonth + thisDay;
-
     console.log(endYear);
     return endYear;
 }
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+url += '?' + $.param({
+    'api-key': "4bd4b70fd6a647289476db64bb764974",
+    'q': searchTerm,
+    'begin_date': startYear,
+    'end_date': endYear
+});
+$.ajax({
+    url: url,
+    method: 'GET',
+}).done(function(result) {
+    console.log(url);
+    console.log(result);
+}).fail(function(err) {
+    throw err;
+});
+
+$('#').on('click',function() {
+    searchTerm = $('#').val().trim();
+    recordNumber = $('#').val().trim();
+    startYear = $('#').val().trim();
+    endYear = $('#').val().trim();
+})
+
+
+$('#').append(result)
+
